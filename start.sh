20fc58fbcaa4518f58747dc24957fb89cb6a0f1e
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-05-17 22:21:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  echo "Main Repository"
+  git clone https://github.com/RoyalKrrishna/Doluram2.0.git /Doluram
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Doluram
 fi
-cd /EvaMaria
+cd /Doluram
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
